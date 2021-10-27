43c1471872a6bc5413f3f2a2ea531e4a1d1231ea
TahminaChowdhury <tahmina10st@gmail.com>
2021-10-27 13:27:24 +0600

    commit

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const { MongoClient } = require('mongodb');
 const app = express();
+const cors =require("cors");
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
+// username:emaJohnSimple
+// pass: OhbRfiJAk6BDujxj
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// environment variable access korte hbe procces.env. diye access korte hbe.
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ckcl0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get("/", (req,res) => {
     res.send("ema-john server is running")
 });
-
-
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.ckcl0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(port, () => {
     console.log("Running server at port", port)
 });
+
+
